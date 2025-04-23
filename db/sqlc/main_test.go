9437c58a7db426cbdb5554f1c5864a6649498503
thisausername <1279664295@qq.com>
2025-04-23 19:24:31 +0800

    first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *pgxpool.Pool
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	defer testDB.Close()
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
